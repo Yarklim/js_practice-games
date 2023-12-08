298f9006efe3b88446d3d8fba83b50c3580e84ca
Yaroslav Klimas <yarklim@gmail.com>
2023-12-08 13:07:52 +0200

    created fifteen.js

--- js/fifteen.js
+++ js/fifteen.js
@@ -7,4 +7,29 @@ if (buttonItems.length !== countItems) {
   throw new Error(`There should be ${countItems} items in HTML`);
 }
 
-/* ====== Position ======*/
+/* ====== 1 - Position ======*/
+let matrix = getMatrix(buttonItems.map(el => Number(el.dataset.matrixId)));
+
+console.log(matrix);
+/* ====== 2 - Shuffle ======*/
+/* ====== 3 - Change Position by click ======*/
+/* ====== 3 - Change Position by keydown ======*/
+
+/* ====== Helpers ======*/
+function getMatrix(arr) {
+  const matrix = [[], [], [], []];
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (x >= 4) {
+      y++;
+      x = 0;
+    }
+
+    matrix[y][x] = arr[i];
+    x++;
+  }
+
+  return matrix;
+}
