052772a247380f6ab0d8c8f545170cae57cb20d6
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-12-08 10:21:12 +0000

    Deploying to gh-pages from @ Yarklim/js_practice-games@e7622cacff6946dbc3c1d36bea83498ac07f2649 🚀

---
+++ index.8b1b6bc0.js
@@ -0,0 +1,2 @@
+!function(){if(document.querySelector("#fifteen"),16!==Array.from(document.querySelectorAll(".fifteen__button")).length)throw Error("There should be 16 items in HTML")}();
+//# sourceMappingURL=index.8b1b6bc0.js.map
---
+++ index.f71655cd.js
@@ -0,0 +1,2 @@
+if(document.querySelector("#fifteen"),16!==Array.from(document.querySelectorAll(".fifteen__button")).length)throw Error("There should be 16 items in HTML");
+//# sourceMappingURL=index.f71655cd.js.map
