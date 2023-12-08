8490082d0676aaa7eed1b8bc662e9020408b430f
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-12-08 13:40:10 +0000

    Deploying to gh-pages from @ Yarklim/js_practice-games@ff59fbba90bb081f110cbf3ef974361f275825d5 🚀

--- index.b4375010.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#fifteen");const e=Array.from(document.querySelectorAll(".fifteen__button"));if(16!==e.length)throw Error("There should be 16 items in HTML");console.log(function(e){let t=[[],[],[],[]],r=0,o=0;for(let n=0;n<e.length;n++)r>=4&&(o++,r=0),t[o][r]=e[n],r++;return t}(e.map(e=>Number(e.dataset.matrixId))));
-//# sourceMappingURL=index.b4375010.js.map
