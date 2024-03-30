c77c78ddc15566c41b4dd8d66a140a97a095dbd2
Yaroslav Klimas <yarklim@gmail.com>
2024-03-30 23:17:52 +0200

    updated match

--- js/match/item-element.js
+++ js/match/item-element.js
@@ -1,3 +1,31 @@
 export const cardItemEl = value => {
-  return `<li class="match__cards--item">${value}</li>`;
+  return `<li class="match__cards--item">
+  ${value}
+  </li>`;
 };
+// export const cardItemEl = value => {
+//   return `<li class="match__cards--item">
+//   <img src="../assets/images/match/${value}.png"
+//   alt="card"
+//   class="match__card--img"
+//   width="200"
+//   height="200" />
+//   </li>`;
+// };
+
+// export const cardItemEl = value => {
+//   const cardItem = document.createElement('li');
+//   cardItem.classList.add('match__cards--item');
+
+//   const cardImg = document.createElement('img');
+//   cardImg.classList.add('match__card--img');
+//   cardImg.src = `../assets/images/match/${value}.png`;
+//   cardImg.alt = 'card';
+//   cardImg.width = '200';
+//   cardImg.height = '200';
+
+//   cardItem.append(cardImg);
+
+//   return cardItem;
+// };
+// https://drive.google.com/file/d/1tByHXKwubaunwxVE6wfVCLUBPY4aQOr7/view?usp=sharing
