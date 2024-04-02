6bc90ec8ffbeaf0398009797dcdce75d36d01094
Yaroslav Klimas <yarklim@gmail.com>
2024-04-02 16:31:19 +0300

    updated titactoe

---
+++ js/tictactoe/score-count.js
@@ -0,0 +1,3 @@
+const playerScoreEl = document.querySelector('[data-player-value]');
+const compScoreEl = document.querySelector('[data-comp-value]');
+const drawScoreEl = document.querySelector('[data-draw-value]');
